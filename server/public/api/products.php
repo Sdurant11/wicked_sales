0665550b9dd75cd9cce44c6e2bbc7164b32a7167
Sdurant11 <samdurant.teacher@gmail.com>
2019-08-23 18:07:42 +0000

    finished creating connection file for mysl

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -1,22 +1,16 @@
 
-  <?php
+<?php
 
- require_once('functions.php');
+require_once ('db_connection.php');
 
- set_exception_handler ('error_handler');
+  if (!$conn) {
+    die('Connect Error: ' . mysqli_connect_error());
+  }
 
- doStuff();
+require_once('functions.php');
+set_exception_handler ('error_handler');
 
 $output = file_get_contents('dummy-products-list.json');
-
 print($output);
-
-// header('Content-Type: application/json');
-
-// if (empty($_GET['id'])) {
-//   readfile('dummy-products-list.json');
-// } else {
-//   readfile('dummy-product-details.json');
-// }
 
 ?>
