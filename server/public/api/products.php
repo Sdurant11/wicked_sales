c3aca0ed320e00cf09131b5813c1115fa7d6ae41
Sdurant11 <samdurant.teacher@gmail.com>
2019-08-23 19:17:20 +0000

    connected to products table and fetched data

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -1,16 +1,36 @@
 
 <?php
 
-require_once ('db_connection.php');
+require_once('functions.php');
+set_exception_handler('error_handler');
+startup();
+
+require_once('db_connection.php');
 
   if (!$conn) {
     die('Connect Error: ' . mysqli_connect_error());
   }
 
-require_once('functions.php');
-set_exception_handler ('error_handler');
+$query = "SELECT * FROM `products`";
 
-$output = file_get_contents('dummy-products-list.json');
-print($output);
+$result = mysqli_query($conn, $query);
+
+if (!$result) {
+  printf("Connect failed: %s\n", mysqli_connect_error());
+  exit();
+}
+
+$output = [];
+
+while ($row = $result->fetch_assoc()) {
+  array_push($output, $row);
+}
+
+$json_output = (json_encode($output));
+print($json_output);
+
+
+// $output = file_get_contents('dummy-products-list.json');
+// print($output);
 
 ?>
