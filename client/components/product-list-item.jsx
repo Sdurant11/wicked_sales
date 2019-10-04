00908411db11989863d940edd090c25586019abb
Samantha Durant <samdurant.teacher@gmail.com>
2019-10-04 20:14:06 +0000

    changed dummy data content and updated card styles

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -4,13 +4,19 @@ function ProductListItem(props) {
   var price = props.price;
   price = '$' + (price / 100).toFixed(2);
   return (
-    <div className="card mb-5 mx-auto d-flex itemCard" style={{ width: '250px', height: '400px', cursor: 'pointer' }}>
-      <img className="card-img-top p-1" style={{ width: '100%', height: '50%' }} src={props.image} alt="Card image cap"></img>
-      <div className="card-body">
+    <div className="card mb-5 mx-auto d-flex itemCard">
+      <img className="card-img-top" style={{ width: '100%', height: '100%' }} src={props.image} alt="Card image cap"></img>
+      <div className="overlay">
+        <div className="cardText">
+          <div>{props.name}</div>
+          <div>{price}</div>
+        </div>
+      </div>
+      {/* <div className="card-body">
         <h5 className="card-title">{props.name}</h5>
         <h6 className="card-subtitle mb-2 text-muted">{price}</h6>
         <p className="card-text">{props.text}</p>
-      </div>
+      </div> */}
     </div>
   );
 }
