6edaf1bea04f18099284813bd0f660a305c3213f
Samantha Durant <samdurant.teacher@gmail.com>
2019-10-07 03:51:47 +0000

    changed dummy data

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -5,6 +5,9 @@ function Header(props) {
     <React.Fragment>
       <nav className="navbar navbar-light p-0">
         <h1 id="title" className="navbar-brand ml-5">Wicked Sales</h1>
+        <div className="pointer">Headwear</div>
+        <div className="pointer">Clothing</div>
+        <div className="pointer">Accessories</div>
         <div className="m-1" id="cartNum">
           <i className="fas fa-shopping-cart m-1"
             onClick={() => { props.view('cart', {}); }}></i>
