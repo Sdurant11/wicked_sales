5fb66a3060d00efdc645e468edf3254ed9b47161
Samantha Durant <samdurant.teacher@gmail.com>
2019-10-07 20:54:50 +0000

    added more products and three sections to header

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -5,7 +5,7 @@ function ProductListItem(props) {
   price = '$' + (price / 100).toFixed(2);
   return (
     <div className="card mb-5 mx-auto d-flex itemCard">
-      <img className="card-img rounded-0" style={{ width: '100%', height: '100%' }} src={props.image} alt="Card image cap"></img>
+      <img className="card-img rounded-0" style={{ height: '100%' }} src={props.image} alt="Card image cap"></img>
       <div className="overlay">
         <div className="cardText">
           <div>{props.name}</div>
