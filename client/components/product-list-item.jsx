d5b622d75150be7037d4395fd350ea90312e3c84
Samantha Durant <samdurant.teacher@gmail.com>
2019-10-12 18:38:14 +0000

    updated cards

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
 function ProductListItem(props) {
-  var price = props.price;
+  let price = props.price;
   price = '$' + (price / 100).toFixed(2);
   const productImage = props.image;
-  const styleObj = { height: '100%', backgroundImage: { productImage }, backgroundSize: 'contain' };
+  const styleObj = {
+    height: '100%',
+    backgroundImage: `url(${productImage})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    overFlow: 'hidden'
+  };
   return (
-    <div className="card mb-5 mx-auto d-flex itemCard itemCardFrame">
-      <div className="card-img rounded-0" style={ styleObj }></div>
+    <div className="card mb-5 mx-auto d-flex itemCard">
+      <div className="rounded-0" style={ styleObj }></div>
       <div className="overlay">
         <div className="cardText">
           <div>{props.name}</div>
@@ -15,15 +21,6 @@ function ProductListItem(props) {
         </div>
       </div>
     </div>
-    // <div className="card mb-5 mx-auto d-flex itemCard shadow">
-    //   <img className="card-img rounded-0" style={{ height: '100%' }} src={props.image} alt="Card image cap"></img>
-    //   <div className="overlay">
-    //     <div className="cardText">
-    //       <div>{props.name}</div>
-    //       <div>{price}</div>
-    //     </div>
-    //   </div>
-    // </div>
   );
 }
 
