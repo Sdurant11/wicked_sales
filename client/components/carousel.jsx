f2c095d25ca09bb9c9e096bfe21892964d228e77
Samantha Durant <samdurant.teacher@gmail.com>
2019-10-16 22:06:50 +0000

    small changes to carousel

--- client/components/carousel.jsx
+++ client/components/carousel.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 function CarouselHeader() {
   return (
-    <div id="carouselExampleIndicators" className="carousel carousel-fade slide w-100" data-ride="carousel">
+    <div id="carouselIndicators" className="carousel carousel-fade slide w-100" data-ride="carousel">
       <ol className="carousel-indicators">
-        <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        <li data-target="#carouselIndicators" data-slide-to="0" className="active"></li>
+        <li data-target="#carouselIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselIndicators" data-slide-to="2"></li>
       </ol>
       <div className="carousel-inner mh-100">
         <div className="carousel-item active backgroundImage" style={{ backgroundImage: 'url("https://scene360.com/wp-content/uploads/2015/11/american-ghoul-06.jpg")', backgroundPosition: '50% 90%' }}>
@@ -16,11 +16,11 @@ function CarouselHeader() {
         <div className="carousel-item backgroundImage" style={{ backgroundImage: 'url("https://i.etsystatic.com/19186398/r/il/ac8581/1802384229/il_fullxfull.1802384229_98eq.jpg")', backgroundPosition: '0% 20%' }}>
         </div>
       </div>
-      <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <a className="carousel-control-prev" href="#carouselIndicators" role="button" data-slide="prev">
         <span className="carousel-control-prev-icon" aria-hidden="true"></span>
         <span className="sr-only">Previous</span>
       </a>
-      <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+      <a className="carousel-control-next" href="#carouselIndicators" role="button" data-slide="next">
         <span className="carousel-control-next-icon" aria-hidden="true"></span>
         <span className="sr-only">Next</span>
       </a>
